e5a24957ab69c4e35ca4af8f4d8204d32cae717a
ansattz <dvieiras@aol.com>
2022-10-10 19:02:03 -0300

    thinking about the subsystem

--- install.sh
+++ install.sh
@@ -1,65 +1,6 @@
 #!/bin/bash
-echo -n "Enter your github email: "
-read email
-
-if [ -f "$HOME/.ssh/id_rsa.pub" ]; then
-   cat $HOME/.ssh/id_rsa.pub
-else 
-   ssh-keygen -t rsa -b 4096 -C $email
-   eval "$(ssh-agent -s)"
-   ssh-add $HOME/.ssh/id_rsa
-   cat $HOME/.ssh/id_rsa.pub
-fi
-echo -e "\n=======> https://github.com/settings/ssh/new\n"
-echo "Did you register the key?"
-sleep 3
-select ans in "Yes" "No"; do
-   case $ans in
-      Yes )
-         break;;
-      No )
-         exit;;
-   esac
-done
-eval `ssh-agent`
-ssh-add
-
-if [ $(uname -r | sed -n 's/.*\( *Microsoft *\).*/\1/ip') ]; then
-   echo "==== Install Docker Desktop > https://www.docker.com/products/docker-desktop/"
-   exit
-fi
-
-distro=$(grep -E '^(PRETTY_NAME|NAME)=' /etc/os-release)
-if [[ $distro == *'Arch'* ]]; then
-   yes | sudo pacman -S git-lfs docker docker-compose
-   sudo systemctl start docker
-   sudo systemctl enable docker
-
-elif [[ $distro == *'Ubuntu'* ]] || [[ $distro == *'Debian'* ]] || [[ $distro == *Mint* ]]; then
-   sudo apt-get install git-lfs docker docker-compose -y
-elif [[ $distro == *'Fedora'* ]]; then
-   sudo dnf install dnf-plugins-core 
-   sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
-   sudo dnf install docker-ce docker-ce-cli containerd.io git-lfs docker-compose -y
-   sudo systemctl start docker
-   sudo systemctl enable docker
-elif [[ $distro == *'openSUSE'*  ]]; then
-   sudo zypper install docker python3-docker-compose git-lfs
-   sudo systemctl enable docker
-else
-   echo "Distro not found"
-   exit
-fi
-git lfs install
-sudo groupadd docker
-sudo usermod -aG docker $USER
-xhost +local:docker
-
-## INSTALL
-echo ""
-echo "###"
-echo "Choose where you want to store the AUV workspace, leave it blank for $HOME/catkin_ws"
-echo "###"
+installg(){
+echo -e "\nChoose where you want to store the AUV workspace, leave it blank for $HOME/catkin_ws"
 echo -n "mkdir -p $HOME/"
 read workspace
 
@@ -100,3 +41,60 @@ cd utils
 rm -rf pysdf
 git rm --cached pysdf
 git submodule add https://github.com/ufrj-nautilus/pysdf
+}
+
+echo -n "Enter your github email: "
+read email
+
+if [ -f "$HOME/.ssh/id_rsa.pub" ]; then
+   cat $HOME/.ssh/id_rsa.pub
+else 
+   ssh-keygen -t rsa -b 4096 -C $email
+   eval "$(ssh-agent -s)"
+   ssh-add $HOME/.ssh/id_rsa
+   cat $HOME/.ssh/id_rsa.pub
+fi
+echo -e "\n=======> https://github.com/settings/ssh/new\n"
+echo "Did you register the key?"
+sleep 3
+select ans in "Yes" "No"; do
+   case $ans in
+      Yes )
+         break;;
+      No )
+         exit;;
+   esac
+done
+eval `ssh-agent`
+ssh-add
+
+distro=$(grep -E '^(PRETTY_NAME|NAME)=' /etc/os-release)
+if [ $(uname -r | sed -n 's/.*\( *Microsoft *\).*/\1/ip') ]; then
+   sudo apt-get install git-lfs -y
+   git lfs install
+   installg
+   exit
+elif [[ $distro == *'Arch'* ]]; then
+   yes | sudo pacman -S git-lfs docker docker-compose
+   sudo systemctl start docker
+   sudo systemctl enable docker
+elif [[ $distro == *'Ubuntu'* ]] || [[ $distro == *'Debian'* ]] || [[ $distro == *'Mint'* ]]; then
+   sudo apt-get install git-lfs docker docker-compose -y
+elif [[ $distro == *'Fedora'* ]]; then
+   sudo dnf install dnf-plugins-core 
+   sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+   sudo dnf install docker-ce docker-ce-cli containerd.io git-lfs docker-compose -y
+   sudo systemctl start docker
+   sudo systemctl enable docker
+elif [[ $distro == *'openSUSE'*  ]]; then
+   sudo zypper install docker python3-docker-compose git-lfs
+   sudo systemctl enable docker
+else
+   echo "Distro not found"
+   exit
+fi
+git lfs install
+sudo groupadd docker
+sudo usermod -aG docker $USER
+xhost +local:docker
+installg
