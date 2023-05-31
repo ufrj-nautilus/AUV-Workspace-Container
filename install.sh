22cb11995ea4569692055e50a3ae4fa40d3781cd
Matheus de Souza <matheusrodrigues@poli.ufrj.br>
2023-05-31 15:22:51 -0300

    fix: updated some ros pkgs

--- install.sh
+++ install.sh
@@ -15,12 +15,11 @@ git clone git@github.com:ufrj-nautilus/control.git
 git clone git@github.com:ufrj-nautilus/brhue.git $HOME/$workspace/catkin_ws/src/robots/brhue
 git clone git@github.com:ufrj-nautilus/lua.git $HOME/$workspace/catkin_ws/src/robots/lua
 git clone git@github.com:ufrj-nautilus/utils.git
+git clone --recursive https://github.com/ufrj-nautilus/auv_gnc.git
 git clone git@github.com:ufrj-nautilus/auv_messages.git
 git clone git@github.com:ufrj-nautilus/localization.git
 git clone git@github.com:ufrj-nautilus/smach.git
-git clone git@github.com:ufrj-nautilus/auv_ws.git
-git clone --recursive git@github.com:ufrj-nautilus/darknet_ros.git
-git clone --branch noetic-devel git@github.com:tdenewiler/uuv_simulator.git
+git clone git@github.com:ufrj-nautilus/uuv_simulator.git
 cd $HOME/$workspace/catkin_ws/src/utils
 rm -rf pysdf
 git rm --cached pysdf
